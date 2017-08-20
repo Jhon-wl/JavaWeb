da325a0440caaef7741f1ae5d1c0a613749a37a8
mr_wl <13152470525@163.com>
2017-08-20 20:09:27 +0800

    resp.setcontenttype()

--- src/main/java/com/web/servlet/ServletTest1.java
+++ src/main/java/com/web/servlet/ServletTest1.java
@@ -5,6 +5,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
 
 /**
  * Create by mr.wl on 2017/8/19
@@ -12,11 +14,42 @@ import java.io.IOException;
 public class ServletTest1 extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("post");
+
+        String name = req.getParameter("name");
+        String paaaword = req.getParameter("pwd");
+        String a = req.getContextPath();
+        String b = req.getLocalAddr();
+        String url = req.getRequestURI();
+
+        String addr = req.getRemoteAddr();
+
+        System.out.println(name+"  "+paaaword);
+        System.out.println(a+"--"+b+"--"+url+"--"+addr);
     }
+
+
+
+
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("get");
+        //tomcat iso 8859-1  默认编码
+        String name = new String(req.getParameter("nameP").getBytes("ISO-8859-1"),"UTF-8");
+        String paaaword = req.getParameter("pwdP");
+
+        resp.setContentType("text/html,charset=UTF-8");
+        PrintWriter out = resp.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>哈哈</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println(name+"------"+paaaword);
+        out.println(new Date());
+        out.println("</body>");
+        out.println("</html>");
+        out.close();
+        System.out.println(name+"  "+paaaword);
     }
+
 }
